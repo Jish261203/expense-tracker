f569a1b03f2924aa60aaf080dd048639df9d6ccc
Jishnay Nair <jish261203@gmail.com>
2024-08-03 22:56:50 +0530

    Connected MongoDB

--- backend/db/connectDB.js
+++ backend/db/connectDB.js
@@ -3,9 +3,9 @@ import mongoose from "mongoose";
 export const connectDB = async () => {
 	try {
 		const conn = await mongoose.connect(process.env.MONGO_URI);
-		console.log(`MongoDB Connected:${conn.connected.host}`);
+		console.log(`MongoDB Connected:${conn.connection.host}`);
 	} catch (err) {
-		console.log(`Error ${err.message}`);
+		console.log(`Error: ${err.message}`);
 		process.exit(1);
 	}
 };
